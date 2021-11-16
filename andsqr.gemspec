a2b83f3baa230de640cdd91a47e902a01e0aa7e8
C.J.Kinniburgh <chris@cjkinni.com>
2021-11-16 11:56:33 -0600

    Add Range option for Andsqr

--- andsqr.gemspec
+++ andsqr.gemspec
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Gem::Specification.new do |s|
-    s.name		= 'andsqr'
-    s.version	= '0.0.2'
-    s.date		= '2017-08-11'
-    s.summary	= 'An AndAnd style gem for dealing with nested Arrays and Hashes'
-    s.authors	= ['Chris Kinniburgh']
-    s.email		= 'andsqr@cjkinni.com'
-    s.files		= ['lib/andsqr.rb']
-    s.homepage	= 'http://github.com/cjkinni/andsqr'
-    s.license	= 'MIT'
+  s.name	= 'andsqr'
+  s.version	= '0.0.3'
+  s.date	= '2021-11-16'
+  s.summary	= 'An AndAnd style gem for dealing with nested Arrays and Hashes'
+  s.authors	= ['C. Kinniburgh']
+  s.email		= 'andsqr@cjkinni.com'
+  s.files		= ['lib/andsqr.rb']
+  s.homepage	= 'http://github.com/cjkinni/andsqr'
+  s.license	= 'MIT'
 end
--- lib/andsqr.rb
+++ lib/andsqr.rb
@@ -1,29 +1,29 @@
+# frozen_string_literal: true
+
 class Hash
-    def andsqr(args=[])
-        if self
-            r = self
-            args.each_with_index do |a, i|
-                return nil if r[a].nil? || (r.class != Hash && r.class != Array)
-                r = r[a]
-            end
-            r
-        else
-            nil
-        end
+  def andsqr(args = [])
+    return nil unless self
+
+    r = self
+    args.each_with_index do |a, _i|
+      return nil if r[a].nil? || (r.class != Hash && r.class != Array)
+
+      r = a.instance_of?(Range) ? a.map { |i| r[i] } : r[a]
     end
+    r
+  end
 end
 
 class Array
-    def andsqr(args=[])
-        if self
-            r = self
-            args.each_with_index do |a, i|
-                return nil if r[a].nil? || (r.class != Hash && r.class != Array)
-                r = r[a]
-            end
-            r
-        else
-            nil
-        end
+  def andsqr(args = [])
+    return nil unless self
+
+    r = self
+    args.each_with_index do |a, _i|
+      return nil if r[a].nil? || (r.class != Hash && r.class != Array)
+
+      r = a.instance_of?(Range) ? a.map { |i| r[i] } : r[a]
     end
+    r
+  end
 end
